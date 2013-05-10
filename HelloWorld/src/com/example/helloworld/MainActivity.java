721f52a9dabe69f65391b669842ea9f24811abe3
Enlin <jannleno1@gmail.com>
2013-05-09 21:36:55 -0400

    Testing

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("test");
         setContentView(R.layout.activity_main);
     }
 
